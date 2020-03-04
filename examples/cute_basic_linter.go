16523e925369fc4cd7af9354de23e8261a70e9c1
Rachel Dowavic <rachel.d@covergenius.com>
2020-03-04 17:40:05 +1100

    added cute basic linter

--- examples/cute_basic_linter.go
+++ examples/cute_basic_linter.go
@@ -9,7 +9,17 @@ import (
 
 func main() {
 	linter := kubelint.NewDefaultLinter()
+	// linter configuration
 	linter.AddV1PodSpecRule(kubelint.V1_PODSPEC_CORRECT_USER_GROUP_ID, kubelint.V1_PODSPEC_NON_NIL_SECURITY_CONTEXT)
+	linter.AddInterdependentRule(&kubelint.InterdependentRule{
+		ID: "INTERDEPENDENT_MANY_RESOURCES",
+		Condition: func(resources []*kubelint.Resource) bool {
+			return len(resources) > 5
+		},
+		Message: "There are only a few resources in this unit",
+		Level:   log.InfoLevel,
+	})
+	linter.Add
 	results, errs := linter.Lint("example_yamls/deployment_invalid_user_group_ids.yaml")
 	for _, err := range errs {
 		log.Error(err)
